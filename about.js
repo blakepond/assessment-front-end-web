ed2aa080ffe039182289972adf301c58a7c68ea2
Blake Pond <blakehp@icloud.com>
2021-10-29 12:29:48 -0600

    assessment-front-end-web complete

--- about.js
+++ about.js
@@ -5,10 +5,15 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("Submitted Successfully")
 }
-
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let compliment = document.getElementById("cat")
+
+compliment.addEventListener("mouseover", function (event){
+	alert("You're beauitful :')")
+})
---
+++ profile.js
@@ -0,0 +1,16 @@
+console.log("work you motha !@#$")
+
+const color = document.getElementById("color")
+const place = document.getElementById("place")
+const ritual = document.getElementById("ritual")
+
+
+color.addEventListener("click", (evt) => {
+    alert("Green and Orange!")
+})
+place.addEventListener("click", (evt) => {
+    alert("The Tetons!")
+})
+ritual.addEventListener("click", (evt) => {
+    alert("Playing Mandolin!")
+})
